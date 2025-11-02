a3ef62ee219bb5de22caa041a30373ff2f7331da
abhishek1349 <krabhi824125@gmail.com>
2025-11-02 23:31:45 +0530

    about basic of variable

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+# Typecasting in Python is the process of converting a variable from one data type to another.
+
+name="Abhishek"
+age=22
+gpa=3.8
+#ypye() is used to check the data type of a variable
+print(type(name))  # Output: <class 'str'>
+print(type(age))   # Output: <class 'int'>
+print(type(gpa))   # Output: <class 'float'>
---
+++ variable.py
@@ -0,0 +1,17 @@
+# Variables in Python
+x = 10          # Integer variable
+y = 3.14        # Float variable
+first_name= "Abhishek"  # String variable
+food="Pizza"      # String variable
+email="H1M6H@example.com"  # String variable
+is_student=True  # Boolean variable
+print(x, y, is_student)
+print(f"Hello {first_name}")
+print(f"I like {food}")
+print(f"My email is {email}")
+#boolean variable indicates True or False values
+for_sale=  False
+if for_sale:
+    print("The item is for sale.")
+else:
+    print("The item is not for sale.")
